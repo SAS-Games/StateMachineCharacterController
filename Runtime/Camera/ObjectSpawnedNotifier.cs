3d90b937de43faea2e6c83b59cf2567f4ee97095
Abhi86shek <sonkar.abhishek86@gmail.com>
2024-08-01 23:03:03 +0530

    meta update

--- Runtime/Camera/ObjectSpawnedNotifier.cs
+++ Runtime/Camera/ObjectSpawnedNotifier.cs
@@ -7,7 +7,7 @@ public class ObjectSpawnedNotifier : MonoBehaviour
 {
     private void Start()
     {
-        var listeners = FindObjectsOfType<MonoBehaviour>().OfType<IObjectSpawnedListener>();
+        var listeners = Object.FindObjectsByType<MonoBehaviour>(FindObjectsSortMode.None).OfType<IObjectSpawnedListener>();
         foreach (var listener in listeners)
             listener.OnSpawn(gameObject);
     }
