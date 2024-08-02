b890e74d858b0f958d2170fda10d339b0510c36c
Abhi86shek <sonkar.abhishek86@gmail.com>
2024-08-02 19:30:51 +0530

    fixed rotation

--- Runtime/Scripts/Actions/ApplyRotation.cs
+++ Runtime/Scripts/Actions/ApplyRotation.cs
@@ -12,11 +12,13 @@ namespace SAS.StateMachineCharacterController
         private ScriptableReadOnlyFloat _turnSmoothTime;
 
         private float _turnSmoothSpeed;
+        private float _minMoveDistance;
 
         void IStateAction.OnInitialize(Actor actor, Tag tag, string key)
         {
             actor.Initialize(this);
             actor.TryGet(out _turnSmoothTime, key);
+            _minMoveDistance = actor.GetComponent<CharacterController>().minMoveDistance;
         }
 
         void IStateAction.Execute(ActionExecuteEvent executeEvent)
@@ -24,7 +26,7 @@ namespace SAS.StateMachineCharacterController
             Vector3 horizontalMovement = _fsmCharacterController.movementVector;
             horizontalMovement.y = 0f;
 
-            if (horizontalMovement.sqrMagnitude >= 0.02f)
+            if (horizontalMovement.sqrMagnitude >= _minMoveDistance)
             {
                 float targetRotation = Mathf.Atan2(_fsmCharacterController.movementVector.x, _fsmCharacterController.movementVector.z) * Mathf.Rad2Deg;
                 _transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(_transform.eulerAngles.y, targetRotation, ref _turnSmoothSpeed, _turnSmoothTime.value);
