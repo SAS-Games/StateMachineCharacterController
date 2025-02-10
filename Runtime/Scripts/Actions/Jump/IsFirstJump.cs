629be76b4ec5aee62bf1659de4cb615af01349f3
Abhishek Sonkar <sonkar.abhishek86@gmail.com>
2025-02-10 23:17:56 +0530

    fixed coyote jump condition

---
+++ Runtime/Scripts/Actions/Jump/IsFirstJump.cs
@@ -0,0 +1,30 @@
+using SAS.StateMachineGraph;
+using SAS.Utilities.BlackboardSystem;
+using SAS.Utilities.TagSystem;
+
+namespace SAS.StateMachineCharacterController
+{
+    public class IsFirstJump : ICustomCondition
+    {
+        private BlackboardKey _remainingJumpCountKey = default;
+        private BlackboardKey _maxJumpCountKey = default;
+        private Actor _actor;
+
+        void ICustomCondition.OnInitialize(Actor actor)
+        {
+            actor.Initialize(this);
+            _actor = actor;
+            _remainingJumpCountKey = actor.GetOrRegisterKey(FSMCharacterBlackboardKey.RemainingJumpCount);
+            _maxJumpCountKey = actor.GetOrRegisterKey(FSMCharacterBlackboardKey.MaxJumpCount);
+        }
+
+        void ICustomCondition.OnStateEnter() { }
+
+        void ICustomCondition.OnStateExit() { }
+
+        bool ICustomCondition.Evaluate()
+        {
+            return _actor.GetValue<int>(_remainingJumpCountKey) == _actor.GetValue<int>(_maxJumpCountKey);
+        }
+    }
+}
